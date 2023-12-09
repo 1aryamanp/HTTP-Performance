171ced12adf03c964102491e6a954beea9fec23e
Aryaman Patel <ap.selfihealth@gmail.com>
2023-12-08 21:47:21 -0500

    Updated files

--- measure-webserver.py
+++ measure-webserver.py
@@ -19,47 +19,43 @@ def calculate_percentiles(latencies):
     return percentiles
 
 def process_packets(pcap_filename, server_ip, server_port):
-    number_of_packets_total = 0
-    number_of_tcp_packets = 0
     http_requests = {}
-
-    processed_file = rdpcap(pcap_filename)  # read in the pcap file
     latencies = []
 
+    processed_file = rdpcap(pcap_filename)  # read in the pcap file
+
     for packet in processed_file:
-        number_of_packets_total += 1
-
-        if packet.haslayer(TCP):  # check if the packet is a TCP packet
-            number_of_tcp_packets += 1
-
+        if packet.haslayer(TCP) and packet.haslayer(IP) and packet.haslayer(HTTP):
             source_ip = packet[IP].src
             dest_ip = packet[IP].dst
+            source_port = packet[TCP].sport
             dest_port = packet[TCP].dport
 
-            if packet.haslayer(HTTP):  # test for an HTTP packet
-                if HTTPRequest in packet:
-                    # HTTP Request
-                    key = (source_ip, packet[TCP].sport)
-                    http_requests[key] = packet.time
-                elif HTTPResponse in packet:
-                    # HTTP Response
-                    key = (dest_ip, dest_port)
-                    if key in http_requests:
+            if HTTPRequest in packet and dest_ip == server_ip and dest_port == server_port:
+                # HTTP Request
+                key = (source_ip, source_port)
+                http_requests[key] = packet.time
+
+            elif HTTPResponse in packet and source_ip == server_ip and source_port == server_port:
+                # HTTP Response
+                key = (dest_ip, dest_port)
+                if key in http_requests:
+                    try:
                         # Calculate round-trip latency
                         latency = packet.time - http_requests[key]
                         latencies.append(latency)
-
-                        # Remove matched request
-                        del http_requests[key]
+                        del http_requests[key]  # Remove matched request
+                    except KeyError:
+                        pass  # Handle the case where request is not found
 
     return latencies
 
+# Main execution
 if len(sys.argv) != 4:
     print("Usage: measure-webserver.py <input-file> <server-ip> <server-port>")
     sys.exit(1)
 
 pcap_filename, server_ip, server_port = sys.argv[1:4]
-
 latencies = process_packets(pcap_filename, server_ip, int(server_port))
 
 if len(latencies) == 0:
@@ -68,6 +64,5 @@ if len(latencies) == 0:
 
 average_latency = sum(latencies) / len(latencies)
 percentiles = calculate_percentiles(latencies)
-
 print("AVERAGE LATENCY: {:.5f}".format(average_latency))
 print("PERCENTILES: {:.5f} {:.5f} {:.5f} {:.5f} {:.5f}".format(*percentiles))
